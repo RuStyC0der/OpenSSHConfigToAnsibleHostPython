56d81f9e1a534f444d2f11c88871ea7652581809
RuStyC0der <miller_maers@tutanota.com>
2021-06-21 12:37:12 +0300

    init

---
+++ configParse.py
@@ -0,0 +1,23 @@
+import re
+import os
+
+class OpenSSHHost():
+    def __init__(self):
+        pass 
+
+class FileParser():
+    
+    def _parseConfigFile(self, configFilePath=None):
+        pass
+
+    def _parseConfigFolder(self, configFolderPath=None):
+        pass
+
+    def parse(self, path):
+        pass
+
+        
+
+
+if __name__ == '__main__':
+    print(FileParser().parse())
